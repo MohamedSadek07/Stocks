0b60608199ad06d75aecff6c445f02cc60d3a9db
Mohamed Sadek <msadek.c@channels.com.sa>
2026-03-25 02:33:39 +0200

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Mohamed Sadek on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
